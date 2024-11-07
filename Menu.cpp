2edfed8fbe389e4b8158756d47522e7888521f56
Storm611 <Ivan>
2024-11-07 22:46:50 +0300

    Create 1st version of Engine menu

---
+++ Menu.cpp
@@ -0,0 +1,101 @@
+#include "Menu.h"
+
+
+void Engine::Menu::setInitText(sf::Text& text, sf::String str, float xpos, float ypos)
+{
+	text.setFont(font);
+	text.setFillColor(menu_text_color);
+	text.setString(str);
+	text.setCharacterSize(size_font);
+	text.setPosition(xpos, ypos);
+	text.setOutlineThickness(2);
+	text.setOutlineColor(border_color);
+
+}
+
+void Engine::Menu::Aligmenu(int posx)
+{
+	float nullx = 0;
+
+	for (int i = 0; i < max_menu; i++)
+	{
+		switch (posx)
+		{
+		case 0:
+			nullx = 0;
+			break;
+		case 1:
+			nullx = mainMenu[i].getLocalBounds().width;
+			break;
+		case 2:
+			nullx = nullx = mainMenu[i].getLocalBounds().width / 2;
+			break;
+		}
+		mainMenu[i].setPosition(mainMenu[i].getPosition().x - nullx, mainMenu[i].getPosition().y);
+
+	}
+
+}
+Engine::Menu::Menu(sf::RenderWindow& window, float menux, float menuy, sf::String name[], int sizeFont, int step)
+	:mywindow(window), menu_X(menux), menu_Y(menuy), size_font(sizeFont), menu_step(step)
+{
+	if (!font.loadFromFile("C:/Users/Admin/Desktop/Arial/arialmt.ttf")) exit(32);
+	max_menu = name->getSize() - 1;
+	mainMenu = new sf::Text[name->getSize()];
+
+	for (int i = 0, ypos = menu_Y; i < max_menu; i++, ypos += menu_step) setInitText(mainMenu[i], name[i], menu_X, ypos);
+	mainMenuSelected = 0;
+	mainMenu[mainMenuSelected].setFillColor(sf::Color::Yellow);
+
+}
+void Engine::Menu::MoveUp()
+{
+	mainMenuSelected--;
+
+	if (mainMenuSelected >= 0)
+	{
+		mainMenu[mainMenuSelected].setFillColor(chose_text_color);
+		mainMenu[mainMenuSelected + 1].setFillColor(menu_text_color);
+	}
+	else
+	{
+		mainMenu[0].setFillColor(menu_text_color);
+		mainMenuSelected = max_menu - 1;
+		mainMenu[mainMenuSelected].setFillColor(chose_text_color);
+	}
+}
+void Engine::Menu::MoveDown()
+{
+	mainMenuSelected++;
+
+	if (mainMenuSelected < max_menu)
+	{
+		mainMenu[mainMenuSelected-1].setFillColor(menu_text_color);
+		mainMenu[mainMenuSelected].setFillColor(chose_text_color);
+	}
+	else
+	{
+		mainMenu[max_menu-1].setFillColor(menu_text_color);
+		mainMenuSelected = 0;
+		mainMenu[mainMenuSelected].setFillColor(chose_text_color);
+	}
+}
+
+void Engine::Menu::Draw()
+{
+	for (int i = 0; i < max_menu; i++) mywindow.draw(mainMenu[i]);
+}
+
+void Engine::Menu::setColorTextMenu(sf::Color menColor, sf::Color ChoColor, sf::Color BordColor)
+{
+	menu_text_color = menColor;
+	chose_text_color = ChoColor;
+	border_color = BordColor;
+	for (int i = 0; i < max_menu; i++)
+	{
+		mainMenu[i].setFillColor(menu_text_color);
+		mainMenu[i].setOutlineColor(border_color);
+	}
+	mainMenu[mainMenuSelected].setFillColor(chose_text_color);
+}
+
--- main.cpp
+++ main.cpp
@@ -1,24 +1,176 @@
 #include <SFML/Graphics.hpp>
+#include "Menu.h"
+using namespace sf;
+using namespace Engine;
+
+void InitText(Text& mtext, float xpos, float ypos, String str, int size_font = 60,
+    Color menu_text_color = Color::Blue, int bord = 0, Color border_color = Color::Black)
+{
+    mtext.setString(str); // Установка строки текста
+    mtext.setCharacterSize(size_font); // Установка размера шрифта
+    mtext.setFillColor(menu_text_color); // Установка цвета текста
+    mtext.setPosition(xpos, ypos); // Установка позиции текста
+
+    // Если требуется граница, можно добавить ее
+    if (bord > 0) {
+        // Создаем текст с границей
+        Text borderText = mtext; // Копируем текст
+        borderText.setFillColor(border_color); // Устанавливаем цвет границы
+        borderText.setPosition(xpos - bord, ypos - bord); // Сдвигаем текст для границы
+        // Здесь вы можете добавить код для рисования границы, если хотите
+    }
+}
+void EngineStart();
+void Options();
+void About_Engine();
+
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "SFML works!",Style::Default );
+
+    //window.setMouseCursorVisible(false);
+    float width = VideoMode::getDesktopMode().width;
+    float height = VideoMode::getDesktopMode().height;
+
+
+    RectangleShape background(Vector2f(width, height));
+
+    //sf::CircleShape shape(100.f);
+    //shape.setFillColor(sf::Color::Green);
+
+    Texture texture_window;
+    if (!texture_window.loadFromFile("C:/Users/Admin/Desktop/previes.jpg")) return 4;
+    background.setTexture(&texture_window);
+
+    Font font;
+    if (!font.loadFromFile("C:/Users/Admin/Desktop/Arial/arialmt.ttf")) return 5;
+
+    Text Titul;
+    Titul.setFont(font);
+    InitText(Titul, 480, 50, L"HIW-Engine", 150, Color(111, 147, 0),3);
+
+
+    String name_menu[4]{ L"start",L"Settings",L"About Engine",L"Quit" };
+
+
+    Engine::Menu mymenu(window, 950, 350, name_menu, 100, 120);
+    mymenu.setColorTextMenu(Color(0, 114, 187), Color::Red, Color::Black);
+    mymenu.Aligmenu(2);
+
+
+
+    Text Version;
+    Version.setFont(font);
+    InitText(Version, 1520, 980, L"V 0.0.0.2", 100, Color(0, 114, 187), 3);
+    //Engine::Menu Settings();
+    // попробуйка создать что-то для меню
 
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+            if (event.type == sf::Event::KeyReleased)
+            {
+                //выбор пунктика
+                if (event.key.code == Keyboard::Up) { mymenu.MoveUp(); }
+                if (event.key.code == Keyboard::Down) { mymenu.MoveDown(); }
+                if (event.key.code == Keyboard::Return)
+                {
+                    switch (mymenu.getSelectedMenuNumber())
+                    {
+                    case 0: EngineStart(); break;
+                    case 1: Options(); break;
+                    case 2: About_Engine(); break;
+                    case 3: window.close(); break;
 
+                    }
+                }
+            }
+        } 
         window.clear();
-        window.draw(shape);
+        window.draw(background);
+        window.draw(Titul);
+        window.draw(Version);
+        mymenu.Draw();
         window.display();
     }
-
     return 0;
+}
+
+void EngineStart()
+{
+    RenderWindow Play(VideoMode::getDesktopMode(), L"Проект 1-й", Style::Fullscreen);
+
+    RectangleShape background_play(Vector2f(1920, 1080));
+
+    Texture texture_play;
+    if (!texture_play.loadFromFile("C:/Users/Admin/Desktop/previes.jpg")) exit(1);
+    background_play.setTexture(&texture_play);
+    
+    while (Play.isOpen())
+    {
+        Event event_play;
+        while (Play.pollEvent(event_play))
+        {
+            if (event_play.type == Event::KeyPressed);
+            {
+                if (event_play.key.code == Keyboard::Escape) { Play.close(); }
+            }
+        }
+        Play.clear();
+        Play.draw(background_play);
+        Play.display();
+    }
+}
+void Options()
+{
+    RenderWindow Options(VideoMode::getDesktopMode(), L"Настройки", Style::Fullscreen);
+
+    RectangleShape background_opt(Vector2f(1920, 1080));
+    Texture texture_opt;
+    if (!texture_opt.loadFromFile("C:/Users/Admin/Desktop/previes.jpg")) exit(2);
+
+    background_opt.setTexture(&texture_opt);
+    while (Options.isOpen())
+    {
+        Event event_opt;
+        while (Options.pollEvent(event_opt))
+        {
+            if (event_opt.type == Event::Closed) Options.close();
+            if (event_opt.type == Event::KeyPressed)
+            {
+                if (event_opt.key.code == Keyboard::Escape) Options.close();
+            }
+        }
+        Options.clear();
+        Options.draw(background_opt);
+        Options.display();
+    }
+}
+void About_Engine()
+{
+    RenderWindow About(VideoMode::getDesktopMode(), L"О движке", Style::Fullscreen);
+
+    RectangleShape background_ab(Vector2f(VideoMode::getDesktopMode().width, VideoMode::getDesktopMode().height));
+    Texture texture_ab;
+    if (!texture_ab.loadFromFile("C:/Users/Admin/Desktop/previes.jpg")) exit(2);
+
+    background_ab.setTexture(&texture_ab);
+    while (About.isOpen())
+    {
+        Event event_play;
+        while (About.pollEvent(event_play))
+        {
+            if (event_play.type == Event::Closed) About.close();
+            if (event_play.type == Event::KeyPressed)
+            {
+                if (event_play.key.code == Keyboard::Escape) About.close();
+            }
+        }
+        About.clear();
+        About.draw(background_ab);
+        About.display();
+    }
 }
